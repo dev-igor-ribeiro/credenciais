2f5c81fb573b9c787edda3374d35f0273c5dc22b
dev-igor-ribeiro <igorsribeiro13.ir@gmail.com>
2025-12-03 22:53:51 -0300

    Update listar_documentos.php

--- src/listar_documentos.php
+++ src/listar_documentos.php
@@ -1,21 +1,15 @@
 <?php
-echo "LISTAR OK"; exit;
-<?php
 require_once "../db/conexao_motoristas.php";
 
 if (!isset($_GET["motorista_id"])) exit("ID inválido");
 
 $id = intval($_GET["motorista_id"]);
 
-// Caminho correto onde os arquivos são salvos
-$basePath = "../documentos/$id/";
-
 $stmt = $pdo->prepare("
     SELECT arquivo
     FROM documentos_motoristas
     WHERE motorista_id = ?
-      AND arquivo IS NOT NULL
-      AND arquivo <> ''
+      AND LENGTH(TRIM(arquivo)) > 0
     ORDER BY id DESC
 ");
 $stmt->execute([$id]);
@@ -30,30 +24,27 @@ foreach ($docs as $d) {
     $arquivo = $d["arquivo"];
     $ext = strtolower(pathinfo($arquivo, PATHINFO_EXTENSION));
 
-    $url = $basePath . $arquivo;
+    $url = "../documentos/$id/$arquivo";
 
     if (in_array($ext, ["jpg","jpeg","png","gif","webp"])) {
-        // Miniatura da imagem
         echo "
             <div class='doc-item'>
                 <img src='$url' class='doc-thumb'>
-                <a class='doc-link' href='$url' target='_blank'>🖼 Abrir imagem</a>
+                <a href='$url' target='_blank'>🖼 Abrir imagem</a>
             </div>
         ";
     } elseif ($ext === "pdf") {
-        // Miniatura padrão para PDFs
         echo "
             <div class='doc-item'>
                 <img src='../assets/icons/pdf.png' class='doc-thumb'>
-                <a class='doc-link' href='$url' target='_blank'>📄 Abrir PDF</a>
+                <a href='$url' target='_blank'>📄 Abrir PDF</a>
             </div>
         ";
     } else {
-        // Arquivo genérico
         echo "
             <div class='doc-item'>
                 <img src='../assets/icons/file.png' class='doc-thumb'>
-                <a class='doc-link' href='$url' download>⬇ Baixar arquivo</a>
+                <a href='$url' download>⬇ Baixar arquivo</a>
             </div>
         ";
     }
