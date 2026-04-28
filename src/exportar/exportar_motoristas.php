ad1dcae839e3b7fbfa4e98dc585ac3b439808fbc
dev-igor-ribeiro <igorsribeiro13.ir@gmail.com>
2026-04-28 09:59:05 -0300

    Update exportar_motoristas.php

--- src/exportar/exportar_motoristas.php
+++ src/exportar/exportar_motoristas.php
@@ -8,9 +8,9 @@ $output = fopen('php://output', 'w');
 fputcsv($output, ['Nome', 'CNH', 'CPF', 'Data Validade', 'Modelo', 'Placa', 'Credencial', 'Status', 'Dias']);
 
 $query = "SELECT * FROM motoristas ORDER BY nome ASC";
-$result = $conn->query($query);
+$result = $pdo->query($query);
 
-while ($row = $result->fetch_assoc()) {
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
     fputcsv($output, [
         $row['nome'],
         $row['cnh'],
